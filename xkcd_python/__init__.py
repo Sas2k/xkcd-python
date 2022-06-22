9646bd3f6c39cf9022d68a7739ef69fe60756dc1
Sasen Perera <sasen.learnings@gmail.com>
2022-06-22 13:14:35 +0530

    Ver 2.5.0

--- xkcd_python/__init__.py
+++ xkcd_python/__init__.py
@@ -2,7 +2,7 @@ from xkcd import xkcd
 
 Client = xkcd
 
-__version__ = '2.2.0'
+__version__ = '2.5.0'
 __author__ = "Sasen Perera"
 __shot_des__ = "A python wrapper for xkcd.com"
 __description__ = "A wrapper for xkcd.com's API. Built Using Python."
